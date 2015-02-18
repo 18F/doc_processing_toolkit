bc86d9b7cb43eb0ef5695982567e487752eb7df3
ramirezg <gabriel.ramirez@gsa.gov>
2015-02-18 18:15:47 -0500

    adding tests

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class TestDocProcessToolkit(unittest.TestCase):
     def test_get_doc_length(self):
         """
         Tests to ensure that doc length function only captures words with
-        over 3 chars
+        over 3 characters
         """
 
         text = "word words more words"
@@ -16,6 +16,16 @@ class TestDocProcessToolkit(unittest.TestCase):
         text = "12323 word w a9s90s"
         self.assertEqual(dpt.get_doc_length(text), 1)
 
+    def test_check_for_text(self):
+        """
+        Check if check_for_text returns True when document contains text
+        """
+
+        doc_path = "fixtures/record_text.pdf"
+        self.assertTrue(dpt.check_for_text(doc_path))
+
+        doc_path = "fixtures/record_no_text.pdf"
+        self.assertFalse(dpt.check_for_text(doc_path))
 
 if __name__ == '__main__':
     unittest.main()
