ac933154100534ca73e7636eccf427d498de5e53
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-02-23 20:58:17 +0000

    Make project structure look a bit more library-like

---
+++ tests/test_extraction.py
@@ -0,0 +1,31 @@
+import unittest
+from textextraction import doc_process_toolkit as dpt
+
+
+class TestDocProcessToolkit(unittest.TestCase):
+
+    def test_get_doc_length(self):
+        """
+        Tests to ensure that doc length function only captures words with
+        over 3 characters
+        """
+
+        text = "word words more words"
+        self.assertEqual(dpt.get_doc_length(text), 4)
+
+        text = "12323 word w a9s90s"
+        self.assertEqual(dpt.get_doc_length(text), 1)
+
+    def test_check_for_text(self):
+        """
+        Check if check_for_text returns True when document contains text
+        """
+
+        doc_path = "tests/fixtures/record_text.pdf"
+        self.assertTrue(dpt.check_for_text(doc_path))
+
+        doc_path = "tests/fixtures/record_no_text.pdf"
+        self.assertFalse(dpt.check_for_text(doc_path))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ textextraction/doc_process_toolkit.py
@@ -0,0 +1,118 @@
+import glob
+import logging
+import os
+import re
+import subprocess
+
+"""
+The functions below are minimal Python wrappers Ghostscript, Tika, and
+Tesseract. They are intended to simplify converting pdf files into usable text.
+"""
+
+WORDS = re.compile('[A-Za-z]{3,}')
+
+
+def get_doc_length(doc_text):
+    """ Return the length of a document and doc text """
+
+    return len(tuple(WORDS.finditer(doc_text)))
+
+
+def check_for_text(doc_path):
+    """
+    Using `pdffonts` returns True if document has fonts, which in essence
+    means it has text
+    """
+    pdffonts_output = subprocess.Popen(
+        ['pdffonts %s' % doc_path],
+        shell=True,
+        stdout=subprocess.PIPE,
+    )
+    if pdffonts_output.communicate()[0].decode("utf-8").count("\n") > 2:
+        return True
+
+
+def save_text(document, export_path=None):
+    """ Reads document text and saves it to specified export path """
+
+    with open(export_path, 'w') as f:
+        f.write(document)
+
+
+def img_to_text(img_path, export_path=None):
+    """ Uses Tesseract OCR to convert tiff image to text file """
+
+    if not export_path:
+        export_path = img_path.replace(".tiff", '')
+
+    document = subprocess.Popen(
+        args=['tesseract', img_path, export_path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    document.communicate()
+    logging.info("%s converted to text from image", img_path)
+    return export_path
+
+
+def pdf_to_img(doc_path, export_path=None):
+    """ Converts and saves pdf file to tiff image using Ghostscript"""
+
+    if not export_path:
+        export_path = doc_path.replace(".pdf", ".tiff")
+
+    args = 'gs -dNOPAUSE -dBATCH -sDEVICE=tiffg4 -sOutputFile={0} {1}'
+    args = args.format(export_path, doc_path)
+    process = subprocess.Popen(
+        args=[args],
+        shell=True,
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE
+    )
+    process.communicate()
+    logging.info("%s converted to tiff image", doc_path)
+    return export_path
+
+
+def pdf_to_text(doc_path, port=9998):
+    """ Converts a document to text using the Tika server """
+
+    document = subprocess.Popen(
+        args=['nc localhost {0} < {1}'.format(port, doc_path)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True)
+    logging.info("%s converted to text from pdf", doc_path)
+    return document
+
+
+def process_documents(glob_path, port=9998, skip_finished=False):
+    """
+    Converts pdfs to text and uses OCR if the initial attempt fails
+    """
+
+    for doc_path in glob.iglob(glob_path):
+        if os.path.exists(doc_path.replace('.pdf', '.txt')) and skip_finished:
+            logging.info("%s: has already been converted", doc_path)
+        else:
+            extraction_succeeded = None
+            # Check if the document has text
+            if check_for_text(doc_path):
+                doc = pdf_to_text(doc_path=doc_path, port=port)
+                doc_text = doc.stdout.read().decode('utf-8')
+                # Check if text extraction succeeded
+                if get_doc_length(doc_text) > 10:
+                    extraction_succeeded = True
+
+            if extraction_succeeded:
+                save_text(doc_text, doc_path.replace(".pdf", ".txt"))
+            else:
+                img_path = pdf_to_img(doc_path)
+                img_to_text(img_path)
+
+
+if __name__ == '__main__':
+
+    # testing script
+    logging.basicConfig(level=logging.INFO)
+    process_documents('test_docs/*/*.pdf', skip_finished=True)
